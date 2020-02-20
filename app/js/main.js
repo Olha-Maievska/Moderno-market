4d45700ffc78ffc12b24b35c90fdbbf0ac48d9c6
Olha-Maievska <olhamaievska91@gmail.com>
2020-02-20 23:50:58 +0100

    added new-items project

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function() {
 
     $('.rate-star').rateYo({
         rating: 5,
-        starWidth: "12px",
+        starWidth: '12px',
         readOnly: true
     });
 
@@ -11,6 +11,27 @@ $(function() {
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4
+    });
+
+      $('.js-range-slider').ionRangeSlider({
+        type: 'double',
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: '$'
+    });
+
+    $('.icon-th-list').on('click', function() {
+        $('.product__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+    });
+
+    $('.icon-th-large').on('click', function() {
+        $('.product__item').removeClass('list');
+        $('.icon-th-list').removeClass('active');
+        $('.icon-th-large').addClass('active');
     });
 
     var mixer = mixitup('.products__inner-box');
